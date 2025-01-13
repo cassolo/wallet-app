d1504744e543f7db48eaeca9eb3a24e96867b9b8
cassolo <aguscuassolo123@gmail.com>
2025-01-13 15:15:21 -0300

    Instalar axios, crear apiClient y componente de prueba

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <TestApiClient />
+  </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestApiClient from './components/TestApiClient.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TestApiClient,
+  },
+};
 </script>
 
 <style>
