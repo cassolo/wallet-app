a701a0d300bfccaeb12685315ef64a5f18f23deb
cassolo <aguscuassolo123@gmail.com>
2025-01-27 15:35:04 -0300

    Routeo de vista de historial

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import LoginView from '../components/LoginView.vue';
 import DashboardView from '../components/DashboardView.vue';
 import SaleAndPurchaseView from '../components/SaleAndPurchaseView.vue';
+import LogView from '../components/LogView.vue';
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: LoginView },
   { path: '/dashboard', component: DashboardView},
-  { path: '/transaction', component: SaleAndPurchaseView}
+  { path: '/transaction', component: SaleAndPurchaseView},
+  { path: '/log', component: LogView}
  
 ];
 
